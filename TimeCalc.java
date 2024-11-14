22701ca8f10ad4ab48d1321a8bb58c79f95303c9
guy michaeli <guy.michaeli@post.runi.ac.il>
2024-11-14 14:07:50 +0200

    Update TimeCalc.java

--- TimeCalc.java
+++ TimeCalc.java
@@ -4,20 +4,20 @@ public class TimeCalc {
         int MinuteToAdd = Integer.parseInt(args[1]);
         int InputHours = Integer.parseInt("" + args[0].charAt(0)+args[0].charAt(1));
         int InputMin = Integer.parseInt("" + args[0].charAt(3)+args[0].charAt(4));
-        InputMin = InputMin + InputHours*60 + MinuteToAdd;
+        InputMin += InputHours*60 + MinuteToAdd;
         String ZeroM ="";
         String ZeroH = "";
 
-        if (InputMin%10 == 0)
+        if (InputMin%60 < 10)
         {
             ZeroM = "0";
         }
 
-        if (((InputMin/60)%24) == 0)
+        if (((InputMin/60)%24) < 10)
         {
             ZeroH = "0";
         }
-        String NewHour = "" + ZeroH + ((InputMin/60)%24) +":" + InputMin%60 + ZeroM;
+        String NewHour = "" + ZeroH + ((InputMin/60)%24) +":" +ZeroM + "" + InputMin%60;
 
         System.out.println(NewHour);
         
